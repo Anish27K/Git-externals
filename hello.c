70f600ea16479cb3852c8e1475e4cc906ae6788b
Anish Kakkar <142825562+Anish27K@users.noreply.github.com>
2024-12-30 00:07:33 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 int main(){
     printf("Hello World\n");
     printf("This change is made through github and isn't present in our local machine yet\n");
+    printf("This is another change made\n");
     return 0;
 }
